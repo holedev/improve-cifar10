07da18ae665a6f3c19100d8b908b4cdf0644a269
holedev <phanleho002@gmail.com>
2025-07-16 16:42:32 +0700

    feat: visualize loss + acc with charts when training

--- src/config.py
+++ src/config.py
@@ -1,2 +1,5 @@
-num_epochs = 20
-result_path = f"./src/result/HeheNet_{num_epochs}epochs.pth"
+num_epochs = 10
+batch_size = 20
+
+weight_result_path = f"./src/result/HeheNet_{num_epochs}epochs.pth"
+chart_result_folder = f"./src/result/charts"
